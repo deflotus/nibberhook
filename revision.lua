82552543987f420518982678047b1c61cd658809
lotus <98671960+deflotus@users.noreply.github.com>
2022-02-26 18:17:12 -0800

    nvm

--- revision.lua
+++
@@ -1,9 +0,0 @@
-local owner = "deflotus"
-local branch = "main"
-
-local function webImport(file)
-    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/nibberhook/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
-end
-
-local commands = webImport("commands")
-table.foreach(commands, print)
